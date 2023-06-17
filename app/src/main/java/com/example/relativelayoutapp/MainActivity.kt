aa6e6e1874664103361674efa11450e885e13866
LukaszA <baklukasz12@gmail.com>
2023-06-17 20:32:39 +0200

    a

---
+++ app/src/main/java/com/example/relativelayoutapp/MainActivity.kt
@@ -0,0 +1,22 @@
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // below access the UI elements
+
+        <TextView
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="Hello World!"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintTop_toTopOf="parent" />
+
+    }
+}
